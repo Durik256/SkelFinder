9e55461f8d6ff7c3e158c07b2fdd02a9014be2f7
Durik256 <61597745+Durik256@users.noreply.github.com>
2022-12-14 01:07:04 +0200

    Update Skel.cs

--- Skel.cs
+++ Skel.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Drawing;
+using System.Numerics;
 using System.Windows.Forms;
 
 namespace SkelFinder
@@ -10,16 +11,16 @@ namespace SkelFinder
 
         double xRotation = 0.0;
         double yRotation = 0.0;
-        double zRotation = 0.0;
 
-        double zoom = 0;
-        double maxDistance = 0;
+        float zoom = 0;
+        float maxDistance = 0;
 
-        Math3D.Camera camera1 = new Math3D.Camera();
-        Math3D.Point3D skelOrigin;
+        Vector3 camera = new Vector3();
+        Vector3 skelOrigin;
         static Bone[] bones;
 
         Font arial = new Font("Arial", 8);
+        Pen redPen = new Pen(Color.Red, 3);
         SolidBrush brush = new SolidBrush(Color.White);
 
         public double RotateX
@@ -34,18 +35,12 @@ namespace SkelFinder
             set { yRotation = value; }
         }
 
-        public double RotateZ
-        {
-            get { return zRotation; }
-            set { zRotation = value; }
-        }
-
         public void cameraZ(double delta)
         {
             if (delta < 0)
-                camera1.Position.Z += maxDistance / 21;
+                camera.Z += maxDistance / 21;
             else
-                camera1.Position.Z -= maxDistance / 21;
+                camera.Z -= maxDistance / 21;
         }
 
         public Skel(Bone[] _bones)
@@ -53,20 +48,21 @@ namespace SkelFinder
             bones = _bones;
             skelOrigin = CalcOrigin();
 
-            zoom = (double)Screen.PrimaryScreen.Bounds.Width / 1.5;
+            zoom = Screen.PrimaryScreen.Bounds.Width / 1.5f;
             calcZcamera();
         }
 
         public void calcZcamera()
         {
-            camera1.Position = new Math3D.Point3D(skelOrigin.X, skelOrigin.Y, skelOrigin.Z + (maxDistance * 7));
+            camera = new Vector3(skelOrigin.X, skelOrigin.Y, skelOrigin.Z + (maxDistance * 7));
         }
 
-        public Math3D.Point3D CalcOrigin()
+        public Vector3 CalcOrigin()
         {
-            Math3D.Point3D origin = new Math3D.Point3D(0, 0, 0);
-            Math3D.Point3D Max = new Math3D.Point3D(0, 0, 0);
-            Math3D.Point3D Min = new Math3D.Point3D(0, 0, 0);
+            Vector3 origin = new Vector3();
+            Vector3 Max    = new Vector3();
+            Vector3 Min    = new Vector3();
+            
             for (int i = 0; i < bones.Length; i++)
             {
                 //X
@@ -79,9 +75,9 @@ namespace SkelFinder
                 Min.Z = Math.Min(Min.Z, bones[i].Matrix.M43);
                 Max.Z = Math.Max(Max.Z, bones[i].Matrix.M43);
             }
-            origin.X = (Min.X + Max.X) / 2;
+            origin.X =  (Min.X + Max.X) / 2;
             origin.Y = -(Min.Y + Max.Y) / 2;
-            origin.Z = (Min.Z + Max.Z) / 2;
+            origin.Z =  (Min.Z + Max.Z) / 2;
 
             //max 
             maxDistance = Math.Max(maxDistance, Math.Abs(Min.X) + Math.Abs(Max.X));
@@ -97,76 +93,64 @@ namespace SkelFinder
             PointF[] point3D = new PointF[bones.Length]; //Will be actual 2D drawing points
             Point tmpOrigin = new Point(0, 0);
 
-            Math3D.Point3D point0 = new Math3D.Point3D(0, 0, 0); //Used for reference
+            Vector3 point0 = new Vector3(0, 0, 0); //Used for reference
 
             //Set up the skel
-            Math3D.Point3D[] cubePoints = copyOrigVertices();
+            Vector3[] skelPoints = copyOrigVertices();
 
             //Apply Rotations, moving the skel to a corner then back to middle
-            cubePoints = Math3D.Translate(cubePoints, skelOrigin, point0);
-            cubePoints = Math3D.RotateX(cubePoints, xRotation); //The order of these
-            cubePoints = Math3D.RotateY(cubePoints, yRotation); //rotations is the source
-            cubePoints = Math3D.RotateZ(cubePoints, zRotation); //of Gimbal Lock
-            cubePoints = Math3D.Translate(cubePoints, point0, skelOrigin);
+            skelPoints = Math3D.Translate(skelPoints, skelOrigin, point0);
+            skelPoints = Math3D.RotateX(skelPoints, xRotation); //The order of these
+            skelPoints = Math3D.RotateY(skelPoints, yRotation); //rotations is the source
+            skelPoints = Math3D.Translate(skelPoints, point0, skelOrigin);
 
             //Convert 3D Points to 2D
-            Math3D.Point3D vec;
+            Vector3 vec;
             for (int i = 0; i < point3D.Length; i++)
             {
-                vec = cubePoints[i];
-                if (vec.Z - camera1.Position.Z >= 0)
+                vec = skelPoints[i];
+                if (vec.Z - camera.Z >= 0)
                 {
-                    point3D[i].X = (int)((double)-(vec.X - camera1.Position.X) / (-0.1f) * zoom) + drawOrigin.X;
-                    point3D[i].Y = (int)((double)(vec.Y - camera1.Position.Y) / (-0.1f) * zoom) + drawOrigin.Y;
+                    point3D[i].X = (int)(-(vec.X - camera.X) / (-0.1f) * zoom) + drawOrigin.X;
+                    point3D[i].Y = (int)( (vec.Y - camera.Y) / (-0.1f) * zoom) + drawOrigin.Y;
                 }
                 else
                 {
-                    tmpOrigin.X = (int)((double)(skelOrigin.X - camera1.Position.X) / (double)(skelOrigin.Z - camera1.Position.Z) * zoom) + drawOrigin.X;
-                    tmpOrigin.Y = (int)((double)-(skelOrigin.Y - camera1.Position.Y) / (double)(skelOrigin.Z - camera1.Position.Z) * zoom) + drawOrigin.Y;
+                    tmpOrigin.X = (int)( (skelOrigin.X - camera.X) / (skelOrigin.Z - camera.Z) * zoom) + drawOrigin.X;
+                    tmpOrigin.Y = (int)(-(skelOrigin.Y - camera.Y) / (skelOrigin.Z - camera.Z) * zoom) + drawOrigin.Y;
 
-                    point3D[i].X = (float)((vec.X - camera1.Position.X) / (vec.Z - camera1.Position.Z) * zoom + drawOrigin.X);
-                    point3D[i].Y = (float)(-(vec.Y - camera1.Position.Y) / (vec.Z - camera1.Position.Z) * zoom + drawOrigin.Y);
+                    point3D[i].X =  (vec.X - camera.X) / (vec.Z - camera.Z) * zoom + drawOrigin.X;
+                    point3D[i].Y = -(vec.Y - camera.Y) / (vec.Z - camera.Z) * zoom + drawOrigin.Y;
 
                     point3D[i].X = (int)point3D[i].X;
                     point3D[i].Y = (int)point3D[i].Y;
                 }
             }
 
-            Bitmap tmpBmp = new Bitmap(300, 300);
+            Bitmap tmpBmp = new Bitmap(300, 300);//pic3D.Width; pic3D.Height;
             Graphics g = Graphics.FromImage(tmpBmp);
 
             for (int i = 0; i < point3D.Length; i++)
             {
-                if (point3D[i].X > 300 || point3D[i].Y > 300 || point3D[i].X < 0 || point3D[i].Y < 0)
-                    continue;
                 g.FillRectangle(brush, point3D[i].X - 2.5f, point3D[i].Y - 2.5f, 5, 5);
                 if (printName)
                     g.DrawString(bones[i].Name, arial, brush, point3D[i]);//render names
             }
-            Pen redPen = new Pen(Color.Red, 3);
 
             for (int i = 0; i < point3D.Length; i++)
             {
                 if (bones[i].Parent >= 0 && bones[i].Parent < point3D.Length)
-                    try
-                    {
-                        g.DrawLine(redPen, point3D[i], point3D[bones[i].Parent]);
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Error Parent index");
-                    }
+                    g.DrawLine(redPen, point3D[i], point3D[bones[i].Parent]);
             }
 
             g.Dispose(); //Clean-up
-
             return tmpBmp;
         }
-        public static Math3D.Point3D[] copyOrigVertices()
+        public static Vector3[] copyOrigVertices()
         {
-            Math3D.Point3D[] verts = new Math3D.Point3D[bones.Length];
+            Vector3[] verts = new Vector3[bones.Length];
             for (int i = 0; i < bones.Length; i++)
-                verts[i] = new Math3D.Point3D(bones[i].Matrix.M41 * -1, bones[i].Matrix.M42 * -1, bones[i].Matrix.M43);
+                verts[i] = new Vector3(bones[i].Matrix.M41 * -1, bones[i].Matrix.M42 * -1, bones[i].Matrix.M43);
 
             return verts;
         }
