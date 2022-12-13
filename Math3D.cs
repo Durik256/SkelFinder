5731dffe672bd37489e312f72e246062758e44c2
Durik256 <61597745+Durik256@users.noreply.github.com>
2022-12-14 01:02:07 +0200

    Update Math3D.cs

--- Math3D.cs
+++ Math3D.cs
@@ -1,55 +1,11 @@
-﻿using System;
+using System;
+using System.Numerics;
 
 namespace SkelFinder
 {
     internal class Math3D
     {
-        public class Point3D
-        {
-            //The Point3D class is rather simple, just keeps track of X Y and Z values,
-            //and being a class it can be adjusted to be comparable
-            public double X;
-            public double Y;
-            public double Z;
-
-            public Point3D(int x, int y, int z)
-            {
-                X = x;
-                Y = y;
-                Z = z;
-            }
-
-            public Point3D(float x, float y, float z)
-            {
-                X = (double)x;
-                Y = (double)y;
-                Z = (double)z;
-            }
-
-            public Point3D(double x, double y, double z)
-            {
-                X = x;
-                Y = y;
-                Z = z;
-            }
-
-            public Point3D()
-            {
-            }
-
-            public override string ToString()
-            {
-                return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
-            }
-        }
-
-        public class Camera
-        {
-            //For 3D drawing we need a point of perspective, thus the Camera
-            public Point3D Position = new Point3D();
-        }
-
-        public static Point3D RotateX(Point3D point3D, double degrees)
+        public static Vector3 RotateX(Vector3 point3D, double degrees)
         {
             //Here we use Euler's matrix formula for rotating a 3D point x degrees around the x-axis
 
@@ -62,16 +18,16 @@ namespace SkelFinder
             //[ 0   -sin(x) cos(x)]
 
             double cDegrees = (Math.PI * degrees) / 180.0f; //Convert degrees to radian for .Net Cos/Sin functions
-            double cosDegrees = Math.Cos(cDegrees);
-            double sinDegrees = Math.Sin(cDegrees);
+            float cosDegrees = (float)Math.Cos(cDegrees);
+            float sinDegrees = (float)Math.Sin(cDegrees);
 
-            double y = (point3D.Y * cosDegrees) + (point3D.Z * sinDegrees);
-            double z = (point3D.Y * -sinDegrees) + (point3D.Z * cosDegrees);
+            float y = (point3D.Y * cosDegrees) + (point3D.Z * sinDegrees);
+            float z = (point3D.Y * -sinDegrees) + (point3D.Z * cosDegrees);
 
-            return new Point3D(point3D.X, y, z);
+            return new Vector3(point3D.X, y, z);
         }
 
-        public static Point3D RotateY(Point3D point3D, double degrees)
+        public static Vector3 RotateY(Vector3 point3D, double degrees)
         {
             //Y-axis
 
@@ -80,76 +36,52 @@ namespace SkelFinder
             //[-sin(x)   0    cos(x)]
 
             double cDegrees = (Math.PI * degrees) / 180.0; //Radians
-            double cosDegrees = Math.Cos(cDegrees);
-            double sinDegrees = Math.Sin(cDegrees);
+            float cosDegrees = (float)Math.Cos(cDegrees);
+            float sinDegrees = (float)Math.Sin(cDegrees);
 
-            double x = (point3D.X * cosDegrees) + (point3D.Z * sinDegrees);
-            double z = (point3D.X * -sinDegrees) + (point3D.Z * cosDegrees);
+            float x = (point3D.X * cosDegrees) + (point3D.Z * sinDegrees);
+            float z = (point3D.X * -sinDegrees) + (point3D.Z * cosDegrees);
 
-            return new Point3D(x, point3D.Y, z);
+            return new Vector3(x, point3D.Y, z);
         }
 
-        public static Point3D RotateZ(Point3D point3D, double degrees)
+        public static Vector3 Translate(Vector3 points3D, Vector3 oldOrigin, Vector3 newOrigin)
         {
-            //Z-axis
-
-            //[ cos(x)  sin(x) 0]
-            //[ -sin(x) cos(x) 0]
-            //[    0     0     1]
-
-            double cDegrees = (Math.PI * degrees) / 180.0; //Radians
-            double cosDegrees = Math.Cos(cDegrees);
-            double sinDegrees = Math.Sin(cDegrees);
-
-            double x = (point3D.X * cosDegrees) + (point3D.Y * sinDegrees);
-            double y = (point3D.X * -sinDegrees) + (point3D.Y * cosDegrees);
-
-            return new Point3D(x, y, point3D.Z);
+            Vector3 difference = newOrigin - oldOrigin;
+            return points3D += difference;
         }
 
-        public static Point3D Translate(Point3D points3D, Point3D oldOrigin, Point3D newOrigin)
-        {
-            //Moves a 3D point based on a moved reference point
-            Point3D difference = new Point3D(newOrigin.X - oldOrigin.X, newOrigin.Y - oldOrigin.Y, newOrigin.Z - oldOrigin.Z);
-            points3D.X += difference.X;
-            points3D.Y += difference.Y;
-            points3D.Z += difference.Z;
-            return points3D;
-        }
-
-        //These are to make the above functions workable with arrays of 3D points
-        public static Point3D[] RotateX(Point3D[] points3D, double degrees)
+        public static Vector3[] RotateX(Vector3[] points3D, double degrees)
         {
             for (int i = 0; i < points3D.Length; i++)
-            {
                 points3D[i] = RotateX(points3D[i], degrees);
-            }
+            
             return points3D;
         }
 
-        public static Point3D[] RotateY(Point3D[] points3D, double degrees)
+        public static Vector3[] RotateY(Vector3[] points3D, double degrees)
         {
             for (int i = 0; i < points3D.Length; i++)
-            {
                 points3D[i] = RotateY(points3D[i], degrees);
-            }
+            
             return points3D;
         }
 
-        public static Point3D[] RotateZ(Point3D[] points3D, double degrees)
+        public static Vector3[] Translate(Vector3[] points3D, Vector3 oldOrigin, Vector3 newOrigin)
         {
             for (int i = 0; i < points3D.Length; i++)
-            {
-                points3D[i] = RotateZ(points3D[i], degrees);
-            }
-            return points3D;
-        }
-
-        public static Point3D[] Translate(Point3D[] points3D, Point3D oldOrigin, Point3D newOrigin)
-        {
-            for (int i = 0; i < points3D.Length; i++)
-            {
                 points3D[i] = Translate(points3D[i], oldOrigin, newOrigin);
+            
+            return points3D;
+        }
+
+        //Rotate using Quaternion (not used)
+        public static Vector3[] Rotate(Vector3[] points3D, double xRotation, double yRotation)
+        {
+            Quaternion rot = Quaternion.CreateFromYawPitchRoll(-(float)((Math.PI / 180) * yRotation), (float)((Math.PI / 180) * xRotation), 0);
+            for (int i = 0; i < points3D.Length; i++)
+            {
+                points3D[i] = Vector3.Transform(points3D[i], rot);
             }
             return points3D;
         }
