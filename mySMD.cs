3904deb5397ba9ca2dfc67ded4a3df56278bfef0
Durik256 <61597745+Durik256@users.noreply.github.com>
2022-12-15 00:51:27 +0200

    Update mySMD.cs

--- mySMD.cs
+++ mySMD.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SkelFinder
 {
